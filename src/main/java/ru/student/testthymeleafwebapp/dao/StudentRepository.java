ba3a08fba1c0ad33cc6019d60369e4f000f685d0
ailzakmi <anton_2000_19@mail.ru>
2023-10-09 18:49:04 +0500

    Vse

---
+++ src/main/java/ru/student/testthymeleafwebapp/dao/StudentRepository.java
@@ -0,0 +1,6 @@
+package ru.student.testthymeleafwebapp.dao;
+
+import ru.student.testthymeleafwebapp.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface StudentRepository extends JpaRepository<Student, Long>{
+}
---
+++ src/test/java/ru/student/testthymeleafwebapp/TestThymeleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.student.testthymeleafwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThymeleafWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
